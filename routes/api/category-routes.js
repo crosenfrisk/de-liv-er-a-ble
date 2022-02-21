cae13ff90eb1f793b53a3eac1fae5e2b151094e2
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-21 16:30:37 -0600

    Tag routes complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,7 +29,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name']
+        attributes: ['id', 'product_name']
       }
     ]
   }).then(dbCategoryData => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,6 +6,8 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // get all products
 router.get("/", (req, res) => {
   Product.findAll({
+    // find all products
+    // be sure to include its associated Category and Tag data
     attributes: ["id", "product_name", "price", "stock", "category_id"],
     include: [
       {
