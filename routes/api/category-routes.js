7ae35e1c84b12cee1ca42c5d7c16ee686a6a5985
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-21 21:38:35 -0600

    changed product to category in category-routes, found during video walk through!

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
   })
     .then((dbCategoryData) => {
       if (!dbCategoryData) {
-        res.status(404).json({ message: 'No product found with this id' });
+        res.status(404).json({ message: 'No category found with this id' });
         return;
       }
       res.json(dbCategoryData);
